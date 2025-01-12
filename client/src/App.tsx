3bdd550f91e17979e3f343e09765345f29242d42
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-01-12 13:31:26 +0000

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+// import { useState, useEffect } from "react";
+import "./App.css";
+import Interface from "./Interface";
+// import axios from "axios";
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // const [fruits, setFruits] = useState<string[]>([]);
+  // const fetchApi = async () => {
+  //   const response = await axios.get("http://localhost:3000/api");
+  //   console.log(response);
+  //   setFruits(response.data.fruits);
+  // };
+  // useEffect(() => {
+  //   fetchApi();
+  // }, []);
+
+  return (
+    <>
+      <Interface></Interface>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const OpenAI = require("openai");
+const dotenv = require("dotenv");
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+
+dotenv.config();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/recipe-search", (req, res) => {
+  res.json({ fruits: ["apple", "mango", "orange"] });
+});
+
+// Post request for getting ingredients and then sending those to the api for a response
+
+app.post("/recipe-search", async (req, res) => {
+  const ingredients = req.body.items;
+  // console.log("just checking", ingredients[0], ingredients[1], ingredients[2]);
+
+  console.log("THESE ARE ALL THE INGREDIENTS BEFORE API CALL", ingredients);
+
+  // =============================================================//
+
+  const token = process.env["GITHUB_TOKEN"];
+  async function main(ingredients) {
+    console.log(
+      "these are the ingredients inside the api function : ",
+      ingredients
+    );
+    const client = new OpenAI({
+      baseURL: "https://models.inference.ai.azure.com",
+      apiKey: token,
+    });
+
+    try {
+      const response = await client.chat.completions.create({
+        messages: [
+          { role: "system", content: "" },
+          {
+            role: "user",
+            content: `i have 3 ingredients tell me what sort of recipes can i make with them and also give instructions on how to make those ${ingredients[0]},${ingredients[1]} and ${ingredients[2]} also give only 3 recipes and return a json with the headings as properties and directly give me the json wihtout saying anything else and it should have an array of recipes objects each containing different recipes with ingredients and instructions as arrays and a recipe name as a property keep that same json structure everytime `,
+          },
+        ],
+        model: "gpt-4o",
+        temperature: 1,
+        max_tokens: 4096,
+        top_p: 1,
+      });
+
+      // Send response to the client
+
+      // Cleaning up the text and removing * and #
+
+      const text = response.choices[0].message.content;
+      console.log("raw text : ", text);
+      const cleanUp = (text) => {
+        return text.replace(/[#*-]/g, "");
+      };
+      // const recipes = cleanUp(text);
+      // const recipes = cleanUp(text)
+      //   .split("\n\n\n")
+      //   .map((recipe) => recipe.trim());
+
+      // console.log("This is the text after cleanup : ", cleanUpText);
+      const recipes = cleanUp(text);
+      let cleanedResponse = recipes.replace(/^```json\n|\n```$/g, "").trim();
+
+      console.log(cleanedResponse);
+      const recipeObj = JSON.parse(cleanedResponse);
+
+      res.status(201).json(recipeObj);
+    } catch (error) {
+      // Handling error response
+
+      console.error("The sample encountered an error:", error);
+      res.status(500).json({ error: "failed to get the recipe from the api" });
+    }
+  }
+  main(ingredients);
+
+  // =================================================//
+});
+
+app.listen(3000, () => {
+  console.log("server started listening at 3000.");
+});
+
+// module.exports = { allIngredients };
