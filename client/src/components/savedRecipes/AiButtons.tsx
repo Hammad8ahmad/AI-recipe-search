6bd9d55f66b9b032e14797e91278e08bd6aff056
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-03-01 18:24:03 +0000

    configuring docker

--- client/src/components/savedRecipes/AiButtons.tsx
+++ client/src/components/savedRecipes/AiButtons.tsx
@@ -34,7 +34,7 @@
 
 const AiButtons = ({ recipe, handleAnalysis, handleOptimization, handleInstructions }: any) => {
   return (
-    <div className="flex justify-between items-center gap-2 flex-wrap sm:flex-nowrap w-full">
+    <div className="flex justify-between items-center gap-2 flex-wrap sm:flex-nowrap w-full ">
       <button
         onClick={() => handleAnalysis(recipe)}
         className="relative px-4 py-2 text-white font-medium rounded-lg bg-gradient-to-r from-blue-500 to-indigo-600 overflow-hidden shadow-lg hover:from-indigo-600 hover:to-blue-500 transition-all duration-300 ease-in-out w-full sm:w-auto"
