2f270d9a884bfdb4e78313cdfeb968d096511669
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-03-21 10:50:33 +0000

    added dockerignore file

--- server/Routes/edamamApiRoute.js
+++ server/Routes/edamamApiRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getRecipesFromEdamamApi } = require("../controllers/apiController");
 
-// Post route for getting ingredients and fetching recipes from OpenAI
+// Post route for getting ingredients and fetching recipes from Edamam
 router.post("/",getRecipesFromEdamamApi);
 
 module.exports = router;
