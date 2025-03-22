6dd222765a8cb00a76194cd57847a751730f3159
Hammad8ahmad <hammadahmdd8@gmail.com>
2025-03-22 10:53:39 +0000

    testing site

--- server/server.js
+++ server/server.js
@@ -47,5 +47,7 @@ app.use(errorHandler);
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server started listening at port ${PORT}.`);  
+  console.log(`Server started listening at port ${PORT}.`);
+  
+  
 });
