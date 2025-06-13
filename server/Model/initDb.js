b8ab773c28b51fcdedb601397591f58ae8f524de
Hammad8ahmad <hammad.27a@gmail.com>
2025-06-13 11:16:22 +0500

    added db initialization

---
+++ server/Model/initDb.js
@@ -0,0 +1,24 @@
+// initDb.js
+const pool = require('./db');
+
+const initializeDB = async() => {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS recipes (
+      id SERIAL PRIMARY KEY,
+      label VARCHAR(255) NOT NULL,
+      ingredients JSONB NOT NULL,
+      calories TEXT NOT NULL,
+      image_url TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+
+  try {
+    await pool.query(createTableQuery);
+  } catch (error) {
+    console.log("Error creating table : ", error)
+  }
+
+}
+
+module.exports = initializeDB()
